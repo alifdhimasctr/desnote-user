5cee3a452302e18aaabd6c42d059a4d2a7861699
Alif Dhimas <alifdhimasctr@gmail.com>
2024-04-17 16:39:55 +0700

    update dashboard

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -8,12 +8,12 @@ import { useCookies } from "react-cookie";
 import Link from "next/link";
 
 export default function page() {
-
-  const todayDate = new Date().toISOString().split('T')[0];
-  const tomorrowDate = new Date(new Date().getTime() + 24 * 60 * 60 * 1000).toISOString().split('T')[0];
+  const todayDate = new Date().toISOString().split("T")[0];
+  const tomorrowDate = new Date(new Date().getTime() + 24 * 60 * 60 * 1000)
+    .toISOString()
+    .split("T")[0];
   console.log(todayDate);
   console.log(tomorrowDate);
-
 
   const token = useCookies(["token"]);
   const [todayMeetData, setTodayMeetData] = useState([]);
@@ -83,25 +83,35 @@ export default function page() {
               <a className="font-semibold text-left">Today's Meet</a>
             </div>
 
-            {
-              loading ? (
-                <div className="flex-col bg-gray-100 rounded-lg p-2 my-2">
-                  <a className="font-semibold">Loading...</a>
+            {loading ? (
+              <div>
+                <div className="flex animate-pulse flex-col bg-gray-100 rounded-lg p-2 gap-2 my-2">
+                  <div class="h-5 w-1/4 bg-gray-300 rounded"></div>
+                  <div class="h-4 w-1/12 bg-gray-300 rounded"></div>
                 </div>
-              ) : null
-            }
+
+                <div className="flex animate-pulse flex-col bg-gray-100 rounded-lg p-2 gap-2 my-2">
+                  <div class="h-5 w-1/4 bg-gray-300 rounded"></div>
+                  <div class="h-4 w-1/12 bg-gray-300 rounded"></div>
+                </div>
+
+                <div className="flex animate-pulse flex-col bg-gray-100 rounded-lg p-2 gap-2 my-2">
+                  <div class="h-5 w-1/4 bg-gray-300 rounded"></div>
+                  <div class="h-4 w-1/12 bg-gray-300 rounded"></div>
+                </div>
+              </div>
+            ) : null}
 
             {todayMeetData.map((meet) => (
-                <Link href={`/meeting/edit?id=${meet.idMeet}`}>
-                  <div className="flex-col bg-gray-100 rounded-lg p-2 my-2 hover:bg-gray-200">
+              <Link href={`/meeting/edit?id=${meet.idMeet}`}>
+                <div className="flex-col bg-gray-100 rounded-lg p-2 my-2 hover:bg-gray-200">
                   <a className="font-semibold">{meet.meetTitle}</a>
                   <div className="text-[0.75rem] mt-1 text-gray-500">
                     {new Date(meet.meetDate).toString()}
                   </div>
                 </div>
-                </Link>
-              )
-            )}
+              </Link>
+            ))}
           </div>
 
           <div className="flex max-w-screen gap-4">
@@ -110,19 +120,35 @@ export default function page() {
                 <a className="font-semibold text-left">Upcoming Meet</a>
               </div>
 
-              {
-                upcomingMeetData.slice(0,10).map((meet) => (
-                  <Link href={`/meeting/edit?id=${meet.idMeet}`}>
-                    <div className="flex-col bg-gray-100 rounded-lg p-2 my-2 hover:bg-gray-200">
-                      <a className="font-semibold">{meet.meetTitle}</a>
-                      <div className="text-[0.75rem] mt-1 text-gray-500">
-                        {new Date(meet.meetDate).toString()}
-                      </div>
-                    </div>
-                  </Link>
-                ))
-              }
+              {loading ? (
+                <div>
+                  <div className="flex animate-pulse flex-col bg-gray-100 rounded-lg p-2 gap-2 my-2">
+                    <div class="h-5 w-1/4 bg-gray-300 rounded"></div>
+                    <div class="h-4 w-1/12 bg-gray-300 rounded"></div>
+                  </div>
 
+                  <div className="flex animate-pulse flex-col bg-gray-100 rounded-lg p-2 gap-2 my-2">
+                    <div class="h-5 w-1/4 bg-gray-300 rounded"></div>
+                    <div class="h-4 w-1/12 bg-gray-300 rounded"></div>
+                  </div>
+
+                  <div className="flex animate-pulse flex-col bg-gray-100 rounded-lg p-2 gap-2 my-2">
+                    <div class="h-5 w-1/4 bg-gray-300 rounded"></div>
+                    <div class="h-4 w-1/12 bg-gray-300 rounded"></div>
+                  </div>
+                </div>
+              ) : null}
+
+              {upcomingMeetData.slice(0, 10).map((meet) => (
+                <Link href={`/meeting/edit?id=${meet.idMeet}`}>
+                  <div className="flex-col bg-gray-100 rounded-lg p-2 my-2 hover:bg-gray-200">
+                    <a className="font-semibold">{meet.meetTitle}</a>
+                    <div className="text-[0.75rem] mt-1 text-gray-500">
+                      {new Date(meet.meetDate).toString()}
+                    </div>
+                  </div>
+                </Link>
+              ))}
             </div>
 
             <div className="h-max min-w-96 w-1/2 p-4 flex-col gap-4 bg-white rounded-lg shadow-lg">
@@ -130,49 +156,98 @@ export default function page() {
                 <a className="font-semibold text-left">Finish Meet</a>
               </div>
 
-              {
-                finishedMeetData.slice(0,10).map((meet) => (
-                  <Link href={`/meeting/edit?id=${meet.idMeet}`}>
-                    <div className="flex-col bg-gray-100 rounded-lg p-2 my-2 hover:bg-gray-200">
-                      <a className="font-semibold">{meet.meetTitle}</a>
-                      <div className="text-[0.75rem] mt-1 text-gray-500">
-                        {new Date(meet.meetDate).toString()}
-                      </div>
+              {loading ? (
+                <div>
+                  <div className="flex animate-pulse flex-col bg-gray-100 rounded-lg p-2 gap-2 my-2">
+                    <div class="h-5 w-1/4 bg-gray-300 rounded"></div>
+                    <div class="h-4 w-1/12 bg-gray-300 rounded"></div>
+                  </div>
+
+                  <div className="flex animate-pulse flex-col bg-gray-100 rounded-lg p-2 gap-2 my-2">
+                    <div class="h-5 w-1/4 bg-gray-300 rounded"></div>
+                    <div class="h-4 w-1/12 bg-gray-300 rounded"></div>
+                  </div>
+
+                  <div className="flex animate-pulse flex-col bg-gray-100 rounded-lg p-2 gap-2 my-2">
+                    <div class="h-5 w-1/4 bg-gray-300 rounded"></div>
+                    <div class="h-4 w-1/12 bg-gray-300 rounded"></div>
+                  </div>
+                </div>
+              ) : null}
+
+              {finishedMeetData.slice(0, 10).map((meet) => (
+                <Link href={`/meeting/edit?id=${meet.idMeet}`}>
+                  <div className="flex-col bg-gray-100 rounded-lg p-2 my-2 hover:bg-gray-200">
+                    <a className="font-semibold">{meet.meetTitle}</a>
+                    <div className="text-[0.75rem] mt-1 text-gray-500">
+                      {new Date(meet.meetDate).toString()}
                     </div>
-                  </Link>
-                ))
-              }
+                  </div>
+                </Link>
+              ))}
             </div>
           </div>
 
           <div className="flex max-w-screen gap-4">
-            <div className="h-max min-w-60 w-1/4 p-4 flex-col gap-4 bg-white rounded-lg shadow-lg">
-              <div className="flex align-center justify-between">
-                <a className="font-semibold text-left">Total Meeting</a>
-              </div>
+            {loading ? (
+              <div className="animate-pulse">
+                <div className="h-max min-w-60 w-1/4 p-4 flex-col gap-4 bg-white rounded-lg shadow-lg">
+                  <div className="flex align-center justify-between">
+                    <a className="font-semibold text-left">Total Meeting</a>
+                  </div>
 
-              <div className="flex bg-blue-200 justify-between align-middle rounded-lg p-2 my-2">
-                <a className="font-semibold text-xl self-center">Today</a>
-                <div className="text-3xl font-bold text-black">
-                  {todayMeetData.length}
+                  <div className="flex bg-blue-200 justify-between align-middle rounded-lg p-2 my-2">
+                    <a className="font-semibold text-xl self-center">Today</a>
+                    <div className="text-3xl font-bold text-black">
+                      ...
+                    </div>
+                  </div>
+
+                  <div className="flex bg-yellow-200 justify-between align-middle rounded-lg p-2 my-2">
+                    <a className="font-semibold text-xl self-center">
+                      Upcoming
+                    </a>
+                    <div className="text-3xl font-bold text-black">
+                      ...
+                    </div>
+                  </div>
+
+                  <div className="flex bg-green-200 justify-between align-middle rounded-lg p-2 my-2">
+                    <a className="font-semibold text-xl self-center">Finish</a>
+                    <div className="text-3xl font-bold text-black">
+                      ...
+                    </div>
+                  </div>
                 </div>
               </div>
+            ) : (
+              <div className="h-max min-w-60 w-1/4 p-4 flex-col gap-4 bg-white rounded-lg shadow-lg">
+                <div className="flex align-center justify-between">
+                  <a className="font-semibold text-left">Total Meeting</a>
+                </div>
 
-              <div className="flex bg-yellow-200 justify-between align-middle rounded-lg p-2 my-2">
-                <a className="font-semibold text-xl self-center">Upcoming</a>
-                <div className="text-3xl font-bold text-black">
-                  {upcomingMeetData.length}
+                <div className="flex bg-blue-200 justify-between align-middle rounded-lg p-2 my-2">
+                  <a className="font-semibold text-xl self-center">Today</a>
+                  <div className="text-3xl font-bold text-black">
+                    {todayMeetData.length}
+                  </div>
+                </div>
+
+                <div className="flex bg-yellow-200 justify-between align-middle rounded-lg p-2 my-2">
+                  <a className="font-semibold text-xl self-center">Upcoming</a>
+                  <div className="text-3xl font-bold text-black">
+                    {upcomingMeetData.length}
+                  </div>
+                </div>
+
+                <div className="flex bg-green-200 justify-between align-middle rounded-lg p-2 my-2">
+                  <a className="font-semibold text-xl self-center">Finish</a>
+                  <div className="text-3xl font-bold text-black">
+                    {finishedMeetData.length}
+                  </div>
                 </div>
               </div>
-
-              <div className="flex bg-green-200 justify-between align-middle rounded-lg p-2 my-2">
-                <a className="font-semibold text-xl self-center">Finish</a>
-                <div className="text-3xl font-bold text-black">
-                  {finishedMeetData.length}
-                </div>
-              </div>
-              
-            </div>
+            )}
 
             <div className="h-max min-w-96 w-3/4 p-4 flex-col gap-4 bg-white rounded-lg shadow-lg">
               <div className="flex align-center justify-between">
